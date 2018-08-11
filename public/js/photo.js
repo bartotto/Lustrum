97c32660d138797e18e45243dd299e2e4808def0
Bart Otto <commit@bartotto.com>
2018-08-11 14:05:31 +0200

    Lappland foto's op volgorde

--- public/js/photo.js
+++ public/js/photo.js
@@ -10,5 +10,5 @@ function showSlides() {
     slideIndex++;
     if (slideIndex > slides.length) {slideIndex = 1} 
     slides[slideIndex-1].style.display = "block"; 
-    setTimeout(showSlides, 2500); // Change image every 2,5 seconds
+    setTimeout(showSlides, 2500); // If set to 2500 change image every 2,5 seconds
     }

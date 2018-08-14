7df0273a2814bacfe8a7748a9fdab28a4ab23f8d
Bart Otto <commit@bartotto.com>
2018-08-14 21:27:48 +0200

    Logo's fiksen

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,12 +1,12 @@
 <footer class="bart-footer">
     <div class="container">
         <div>
-            <img src="\img\logos\Kenia.jpg" alt="Kenia" height="auto" width="80">
-            <img src="\img\logos\Lappland.jpg" alt="Kenia" height="auto" width="80">
-            <img src="\img\logos\Cameroun.jpg" alt="Kenia" height="auto" width="80">
-            <img src="\img\logos\Oman.jpg" alt="Kenia" height="auto" width="80">
-            <img src="\img\logos\Karpaten.jpg" alt="Kenia" height="auto" width="80">
-            <img src="\img\logos\Dalarna.jpg" alt="Kenia" height="auto" width="80">
+            <img src="img\logos\Kenia.jpg" alt="Kenia" height="auto" width="80">
+            <img src="img\logos\Lappland.jpg" alt="Kenia" height="auto" width="80">
+            <img src="img\logos\Cameroun.jpg" alt="Kenia" height="auto" width="80">
+            <img src="img\logos\Oman.jpg" alt="Kenia" height="auto" width="80">
+            <img src="img\logos\Karpaten.jpg" alt="Kenia" height="auto" width="80">
+            <img src="img\logos\Dalarna.jpg" alt="Kenia" height="auto" width="80">
         </div>
         <p align="center">{{ trans('info.footer') }}</p>
         <p align="center">

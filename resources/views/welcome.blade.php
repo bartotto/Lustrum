4851a67395a81eecf93851c48383dc5925d1da87
Bart Otto <commit@bartotto.com>
2018-08-16 09:15:26 +0200

    Fix Login als previous_last_login is NULL

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,21 +18,23 @@
                     </div>
                 </div>
             </div>
-            <div class="col-lg-3 col-md-3 col-sm-3">
-                <div class="card card-block">
-                    <img class="card-img-top" src="img/Login.png" alt="Login safely" height="auto" width="58">
-                    <div class="card-header">
-                        <h2>Safety first!</h2>
-                    </div>
-                    <div class="card-body">
-                        <p>U was voor het laatst ingelogd op: {{ Auth::user()->previous_last_login->format('d M Y')}}
-                        om {{ Auth::user()->previous_last_login->format('H i')}}</p>
-                    </div>
-                    <div class="card-footer text-muted">
-                        <p>Wat u dit niet? Verander gauw u wachtwoord of trek onmiddelijk aan de bel bij <a href="mailto:luco@lustrumcaligula.com?SUBJECT=Wachtwoord probleem">Webmaster</a></p>
+            @if(Auth::user()->previous_last_login)
+                <div class="col-lg-3 col-md-3 col-sm-3">
+                    <div class="card card-block">
+                        <img class="card-img-top" src="img/Login.png" alt="Login safely" height="auto" width="58">
+                        <div class="card-header">
+                            <h2>Safety first!</h2>
+                        </div>
+                        <div class="card-body">
+                                <p>U was voor het laatst ingelogd op: {{ Auth::user()->previous_last_login->format('d M Y')}}
+                                om {{ Auth::user()->previous_last_login->format('H i')}}</p>
+                        </div>
+                        <div class="card-footer text-muted">
+                            <p>Wat u dit niet? Verander gauw u wachtwoord of trek onmiddelijk aan de bel bij <a href="mailto:luco@lustrumcaligula.com?SUBJECT=Wachtwoord probleem">Webmaster</a></p>
+                        </div>
                     </div>
                 </div>
-            </div>
+            @endif
         </div>
     </div>
 @endsection

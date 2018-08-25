00abbc723d4c1ba450ff01208e038cacfddfdee6
Bart Otto <bart.otto@hotmail.com>
2018-08-26 00:26:03 +0200

    fixes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,9 @@ class AppServiceProvider extends ServiceProvider {
      *
      * @return void
      */
-    public function register() {
+    /**public function register() {
         //
         }
+     * 
+     */
     }

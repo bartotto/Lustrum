6d92001a0667014cd2ead806a15e7f6788d414d5
Bart Otto <bart.otto@hotmail.com>
2018-08-26 20:37:19 +0200

    fix blueimp Gallery

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,7 +43,7 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>  
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
         <script src="{{ asset('js/app.js') }}"></script>
-        <script src="js/blueimp-gallery.min.js"></script>
+        <script src="js/jquery.blueimp-gallery.min.js"></script>
         <script>
             document.getElementById('links').onclick = function (event) {
                 event = event || window.event;
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -28,7 +28,7 @@
                 <h2>{{ trans('info.pictures_club') }}</h2>
             </div>
             <div class="card-body">
-                <div id="blueimp-gallery" class="blueimp-gallery blueimp-gallery-controls">
+                <div id="blueimp-gallery-club" class="blueimp-gallery blueimp-gallery-controls">
                     <div class="slides"></div>
                     <h3 class="title"></h3>
                     <a class="prev">‹</a>
@@ -38,17 +38,17 @@
                     <ol class="indicator"></ol>
                 </div>
                 <div id="links">
-                    <a href="img\Club\Jaarclub_Rdam.jpg" title="Rotterdam 1983"><img src="img\Club\Thumbnails\Jaarclub_Rdam.jpg" alt="Rotterdam 1983"></a>
-                    <a href="img\Club\Kenya_vuur.jpg" title="Kenya 1989"><img src="img\Club\Thumbnails\Kenya_vuur.jpg" alt="Kenya 1989"></a>
-                    <a href="img\Club\Cameroun_voetbal.jpg" title="Cameroun 1999"><img src="img\Club\Thumbnails\Cameroun_voetbal.jpg" alt="Cameroun 1999"></a>
-                    <a href="img\Club\De_Herten.jpg" title="De Herten later"><img src="img\Club\Thumbnails\De_Herten.jpg" alt="De Herten later"></a>
-                    <a href="img\Club\Oman_kamelen.jpg" title="Oman 2005"><img src="img\Club\Thumbnails\Oman_kamelen.jpg" alt="Oman 2005"></a>
-                    <a href="img\Club\Carpathians_brug.jpg" title="Carpathians 2009"><img src="img\Club\Thumbnails\Carpathians_brug.jpg" alt="Carpathians 2009"></a>
-                    <a href="img\Club\Carpathians_bad.jpg" title="Carpathians 2009"><img src="img\Club\Thumbnails\Carpathians_bad.jpg" alt="Carpathians 2009"></a>
-                    <a href="img\Club\Hangbrug.jpg" title="16 april 2011"><img src="img\Club\Thumbnails\Hangbrug.jpg" alt="16 april 2011"></a>
-                    <a href="img\Club\Carpathians_mevrouw.jpg" title="Carpathians 2009"><img src="img\Club\Thumbnails\Carpathians_mevrouw.jpg" alt="Carpathians 2009"></a>
-                    <a href="img\Club\Dalarna_trek.jpg" title="Dalarna 2014"><img src="img\Club\Thumbnails\Dalarna_trek.jpg" alt="Dalarna 2014"></a>
-                    <a href="img\Club\Dalarna_camp.jpg" title="Dalarna 2014"><img src="img\Club\Thumbnails\Dalarna_camp.jpg" alt="Dalarna 2014"></a>
+                    <a href="img\Club\Jaarclub_Rdam.jpg" title="Rotterdam 1983" data-gallery="#blueimp-gallery-club"><img src="img\Club\Thumbnails\Jaarclub_Rdam.jpg" alt="Rotterdam 1983"></a>
+                    <a href="img\Club\Kenya_vuur.jpg" title="Kenya 1989" data-gallery="#blueimp-gallery-club"><img src="img\Club\Thumbnails\Kenya_vuur.jpg" alt="Kenya 1989"></a>
+                    <a href="img\Club\Cameroun_voetbal.jpg" title="Cameroun 1999" data-gallery="#blueimp-gallery-club"><img src="img\Club\Thumbnails\Cameroun_voetbal.jpg" alt="Cameroun 1999"></a>
+                    <a href="img\Club\De_Herten.jpg" title="De Herten later" data-gallery="#blueimp-gallery-club"><img src="img\Club\Thumbnails\De_Herten.jpg" alt="De Herten later"></a>
+                    <a href="img\Club\Oman_kamelen.jpg" title="Oman 2005" data-gallery="#blueimp-gallery-club"><img src="img\Club\Thumbnails\Oman_kamelen.jpg" alt="Oman 2005"></a>
+                    <a href="img\Club\Carpathians_brug.jpg" title="Carpathians 2009" data-gallery="#blueimp-gallery-club"><img src="img\Club\Thumbnails\Carpathians_brug.jpg" alt="Carpathians 2009"></a>
+                    <a href="img\Club\Carpathians_bad.jpg" title="Carpathians 2009" data-gallery="#blueimp-gallery-club"><img src="img\Club\Thumbnails\Carpathians_bad.jpg" alt="Carpathians 2009"></a>
+                    <a href="img\Club\Hangbrug.jpg" title="16 april 2011" data-gallery="#blueimp-gallery-club"><img src="img\Club\Thumbnails\Hangbrug.jpg" alt="16 april 2011"></a>
+                    <a href="img\Club\Carpathians_mevrouw.jpg" title="Carpathians 2009" data-gallery="#blueimp-gallery-club"><img src="img\Club\Thumbnails\Carpathians_mevrouw.jpg" alt="Carpathians 2009"></a>
+                    <a href="img\Club\Dalarna_trek.jpg" title="Dalarna 2014" data-gallery="#blueimp-gallery-club"><img src="img\Club\Thumbnails\Dalarna_trek.jpg" alt="Dalarna 2014"></a>
+                    <a href="img\Club\Dalarna_camp.jpg" title="Dalarna 2014" data-gallery="#blueimp-gallery-club"><img src="img\Club\Thumbnails\Dalarna_camp.jpg" alt="Dalarna 2014"></a>
                 </div>
             </div>
         </div>

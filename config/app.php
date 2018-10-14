8bcf342be629eb878138bcac4299e95b187d38d9
Bart Otto <bart.otto@hotmail.com>
2018-10-14 22:26:05 +0200

    Barryvdh language alle in require-dev

--- config/app.php
+++ config/app.php
@@ -147,7 +147,6 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
-        'Barryvdh\TranslationManager\ManagerServiceProvider',
 
         /*
          * Package Service Providers...
